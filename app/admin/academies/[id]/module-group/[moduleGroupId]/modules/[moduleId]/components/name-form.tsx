f4369ce318d02ac3a8a94ff169a37cdadef5db04
rezasetyawan <setyawanreza960@gmail.com>
2024-01-11 11:19:28 +0700

    wip(admin module detail)

---
+++ app/admin/academies/[id]/module-group/[moduleGroupId]/modules/[moduleId]/components/name-form.tsx
@@ -0,0 +1,115 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Pencil } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import toast from "react-hot-toast";
+import * as z from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { axiosInstance } from "@/lib/axios";
+import { useForm } from "react-hook-form";
+
+interface TitleFormProps {
+  initialData: {
+    name: string;
+  };
+  academyId: string;
+}
+
+const formSchema = z.object({
+  name: z.string().min(1, {
+    message: "Title is required",
+  }),
+});
+
+const NameForm = ({ initialData, academyId }: TitleFormProps) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [name, setName] = useState(initialData.name);
+
+  const toggleEdit = () => setIsEditing((current) => !current);
+
+  const router = useRouter();
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: initialData,
+  });
+
+  const { isSubmitting, isValid } = form.formState;
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      const updatedAt = Date.now().toString();
+      await axiosInstance.patch(`/academies/${academyId}`, {
+        ...values,
+        updatedAt,
+      });
+      setName(values.name);
+      toast.success("Name updated");
+      toggleEdit();
+    } catch {
+      toast.error("Something went wrong");
+    }
+  };
+
+  return (
+    <div className="mt-6 border bg-slate-100 rounded-md p-4">
+      <div className="font-medium flex items-center justify-between">
+        Academy name
+        <Button onClick={toggleEdit} variant="ghost">
+          {isEditing ? (
+            <>Cancel</>
+          ) : (
+            <>
+              <Pencil className="h-4 w-4 mr-2" />
+              Edit title
+            </>
+          )}
+        </Button>
+      </div>
+      {!isEditing && <p className="text-sm mt-2">{name}</p>}
+      {isEditing && (
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="space-y-4 mt-4"
+          >
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input
+                      disabled={isSubmitting}
+                      placeholder="e.g. 'Advanced web development'"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div className="flex items-center gap-x-2">
+              <Button disabled={!isValid || isSubmitting} type="submit">
+                Save
+              </Button>
+            </div>
+          </form>
+        </Form>
+      )}
+    </div>
+  );
+};
+
+export default NameForm;
--- app/admin/academies/[id]/module-group/[moduleGroupId]/modules/[moduleId]/page.tsx
+++ app/admin/academies/[id]/module-group/[moduleGroupId]/modules/[moduleId]/page.tsx
@@ -1,24 +1,28 @@
-"use client";
+/* eslint-disable @next/next/no-async-client-component */import Editor from "@/components/editor";
 
-import Editor from "@/components/editor";
-import { useEffect, useState } from "react";
+import { Preview } from "@/components/preview";
+import { axiosInstance } from "@/lib/axios";
 
-export default function ModuleDetail() {
+export default async function ModuleDetail({
+    params
+  }: {
+    params: { moduleGroupId: string; moduleId: string }
+  }) {
+  const moduleData = await axiosInstance.get(
+    "/academies/1/module-groups/2/modules/1"
+  );
   let content = "";
-  useEffect(() => {
-    console.log(content);
-  }, [content]);
-
-  const setContent = (value: string) => {
-    content = value;
-    console.log(content);
-  };
+  
+  console.log(moduleData)
+  console.log(params)
   return (
-    <Editor
-      academyId={1}
-      moduleGroupId={1}
-      moduleId={1}
-      setContent={setContent}
-    />
+    <>
+      {/* <Editor
+        academyId={1}
+        moduleGroupId={1}
+        moduleId={1}
+      /> */}
+      <Preview value={moduleData.data.data.content} />
+    </>
   );
 }
