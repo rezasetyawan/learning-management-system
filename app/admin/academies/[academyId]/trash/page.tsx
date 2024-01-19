178286f866893123aa32d12a9a3a1e57953d3d81
rezasetyawan <setyawanreza960@gmail.com>
2024-01-19 07:16:24 +0700

    wip(academy): trash page

--- app/admin/academies/[academyId]/trash/page.tsx
+++ app/admin/academies/[academyId]/trash/page.tsx
@@ -6,6 +6,7 @@ interface ModuleGroupsData {
 
 interface ModulesData {
   data: {
+    name: string;
     moduleGroups: {
       id: string;
       name: string;
@@ -34,17 +35,30 @@ export default async function AcademyDetailTrash({
 
   const modulesData = (await modulesResponse.json()) as ModulesData;
   return (
-    <div>
-      {moduleGroupData.data.map((group) => (
-        <div key={group.id}>{group.name}</div>
-      ))}
-      {modulesData.data.moduleGroups.map((group) => (
-        <div key={group.id}>
-          {group.modules.map((module) => (
-            <div key={module.id}>{module.name}</div>
+    <>
+      <div className="p-4 lg:p-6">
+        <div className="flex items-center justify-between">
+          <div className="flex flex-col gap-y-2">
+            <h1 className="text-lg font-semibold lg:text-2xl">
+              {modulesData.data.name} recycle bin
+            </h1>
+          </div>
+        </div>
+        <div>
+          {moduleGroupData.data.map((group) => (
+            <div key={group.id}>
+                <p>{group.name}</p>
+            </div>
+          ))}
+          {modulesData.data.moduleGroups.map((group) => (
+            <div key={group.id}>
+              {group.modules.map((module) => (
+                <div key={module.id}>{module.name}</div>
+              ))}
+            </div>
           ))}
         </div>
-      ))}
-    </div>
+      </div>
+    </>
   );
 }
