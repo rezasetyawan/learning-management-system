c50350de973ef1ec4c288d6eb57d46d26512710a
rezasetyawan <setyawanreza960@gmail.com>
2024-02-09 20:53:14 +0700

    feat(academy): remove recycle bin navigation and add module trash page navigation

--- app/admin/academies/[academyId]/components/top-section.tsx
+++ app/admin/academies/[academyId]/components/top-section.tsx
@@ -26,10 +26,7 @@ export default function TopSection({
         <Banner label="This academy is unpublished. It will not be visible to the normal users." />
       )}
       <div className="flex items-center justify-between p-4 lg:p-6">
-        <div className="flex flex-col gap-y-2">
-          <h1 className="text-lg font-semibold lg:text-2xl">Academy setup</h1>
-          <Link href={`/admin/academies/${academyId}/trash`}>Recycle bin</Link>
-        </div>
+        <h1 className="text-lg font-semibold lg:text-2xl">Academy setup</h1>
         <Actions
           academyId={academyId}
           isPublished={currentIsPublished}
