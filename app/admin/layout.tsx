48c3e0c340a5bc99d0c8eb515e44deaee04ba79e
rezasetyawan <setyawanreza960@gmail.com>
2024-02-29 16:52:08 +0700

    fix padding top

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,22 +1,16 @@
 import AdminNavbar from "@/components/admin/navbar";
-import Sidebar from "@/components/admin/sidebar"
+import Sidebar from "@/components/admin/sidebar";
 import { cookies } from "next/headers";
 
 interface User {
   role: "admin" | "user" | "superadmin";
 }
 
-
-const DashboardLayout = async ({
-  children
-}: {
-  children: React.ReactNode;
-}) => {
+const DashboardLayout = async ({ children }: { children: React.ReactNode }) => {
   const cookieStore = cookies();
   const accessToken = cookieStore.get("accessToken")?.value || "";
   const data = await fetch(
-    (process.env.NEXT_PUBLIC_API_BASE_URL as string) +
-      "/profile",
+    (process.env.NEXT_PUBLIC_API_BASE_URL as string) + "/profile",
     {
       cache: "no-store",
       headers: {
@@ -25,20 +19,19 @@ const DashboardLayout = async ({
     }
   );
 
-  const user = await data.json() as User
-  return ( 
+  const user = (await data.json()) as User;
+  return (
     <div className="h-full">
-      <div className="h-[80px] lg:pl-56 fixed inset-y-0 w-full z-50">
+      <div className="lg:pl-56 fixed inset-y-0 w-full z-50">
         <AdminNavbar userRole={user.role} />
       </div>
       <div className="hidden lg:flex h-full w-56 flex-col fixed inset-y-0 z-50">
-        <Sidebar userRole={user.role}/>
+        <Sidebar userRole={user.role} />
       </div>
-      <main className="lg:pl-56 pt-[80px] h-full">
-        {children}
-      </main>
+      {/* THIS PADDING TOP BASED ON HEADER HEIGHT */}
+      <main className="pt-14 lg:pt-16 lg:pl-56 h-full">{children}</main>
     </div>
-   );
-}
- 
+  );
+};
+
 export default DashboardLayout;
