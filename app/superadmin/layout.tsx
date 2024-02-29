ffb9ecd24126042d5e2915fb66ad0b47d7ec40df
rezasetyawan <setyawanreza960@gmail.com>
2024-02-29 17:11:44 +0700

    fix headr layout inset

--- app/superadmin/layout.tsx
+++ app/superadmin/layout.tsx
@@ -21,14 +21,14 @@ const DashboardLayout = async ({ children }: { children: React.ReactNode }) => {
   const user = (await data.json()) as User;
   return (
     <div className="h-full">
-      <div className="lg:pl-56 fixed inset-y-0 w-full z-50">
+      <div className="lg:pl-56 fixed inset-x-0 w-full z-50">
         <AdminNavbar userRole={user.role} />
       </div>
       <div className="hidden lg:flex h-full w-56 flex-col fixed inset-y-0 z-50">
         <Sidebar userRole={user.role} />
       </div>
       {/* THIS PADDING TOP BASED ON HEADER HEIGHT */}
-      <main className="pt-14 lg:pt-16 lg:pl-56 h-full">{children}</main>
+      <main className="pt-14 lg:pt-16 lg:pl-56 h-full z-50">{children}</main>
     </div>
   );
 };
