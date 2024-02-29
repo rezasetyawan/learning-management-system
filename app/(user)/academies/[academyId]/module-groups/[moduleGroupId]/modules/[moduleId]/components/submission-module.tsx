5c24f9dc02a51e6d12af45a6530e55cc4b36b248
rezasetyawan <setyawanreza960@gmail.com>
2024-02-29 14:36:36 +0700

    fix svg attribute name

--- app/(user)/academies/[academyId]/module-groups/[moduleGroupId]/modules/[moduleId]/components/submission-module.tsx
+++ app/(user)/academies/[academyId]/module-groups/[moduleGroupId]/modules/[moduleId]/components/submission-module.tsx
@@ -113,9 +113,9 @@ export default function SubmissionModule({
                           viewBox="0 0 24 24"
                           fill="none"
                           stroke="currentColor"
-                          stroke-width="2"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
+                          strokeWidth="2"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
                           className="lucide lucide-timer w-7 h-7"
                         >
                           <line x1="10" x2="14" y1="2" y2="2" />
@@ -142,9 +142,9 @@ export default function SubmissionModule({
                           viewBox="0 0 24 24"
                           fill="none"
                           stroke="currentColor"
-                          stroke-width="2"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
+                          strokeWidth="2"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
                           className="lucide lucide-timer w-7 h-7"
                         >
                           <line x1="10" x2="14" y1="2" y2="2" />
@@ -169,8 +169,8 @@ export default function SubmissionModule({
                           xmlns="http://www.w3.org/2000/svg"
                         >
                           <path
-                            fill-rule="evenodd"
-                            clip-rule="evenodd"
+                            fillRule="evenodd"
+                            clipRule="evenodd"
                             d="M13.6668 7.00065C13.6668 10.6825 10.6821 13.6673 7.00016 13.6673C3.31826 13.6673 0.333496 10.6825 0.333496 7.00065C0.333496 3.31875 3.31826 0.333984 7.00016 0.333984C10.6821 0.333984 13.6668 3.31875 13.6668 7.00065ZM7.00016 3.66732C7.36835 3.66732 7.66683 3.96579 7.66683 4.33398V7.66732C7.66683 8.03551 7.36835 8.33398 7.00016 8.33398C6.63197 8.33398 6.3335 8.03551 6.3335 7.66732V4.33398C6.3335 3.96579 6.63197 3.66732 7.00016 3.66732ZM7.00016 10.334C7.36835 10.334 7.66683 10.0355 7.66683 9.66732C7.66683 9.29913 7.36835 9.00065 7.00016 9.00065C6.63197 9.00065 6.3335 9.29913 6.3335 9.66732C6.3335 10.0355 6.63197 10.334 7.00016 10.334Z"
                           ></path>
                         </svg>
@@ -225,9 +225,9 @@ export default function SubmissionModule({
                           viewBox="0 0 24 24"
                           fill="none"
                           stroke="currentColor"
-                          stroke-width="2"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
+                          strokeWidth="2"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
                           className="lucide lucide-timer w-6 h-6"
                         >
                           <line x1="10" x2="14" y1="2" y2="2" />
@@ -254,9 +254,9 @@ export default function SubmissionModule({
                           viewBox="0 0 24 24"
                           fill="none"
                           stroke="currentColor"
-                          stroke-width="2"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
+                          strokeWidth="2"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
                           className="lucide lucide-timer w-6 h-6"
                         >
                           <line x1="10" x2="14" y1="2" y2="2" />
@@ -281,8 +281,8 @@ export default function SubmissionModule({
                           xmlns="http://www.w3.org/2000/svg"
                         >
                           <path
-                            fill-rule="evenodd"
-                            clip-rule="evenodd"
+                            fillRule="evenodd"
+                            clipRule="evenodd"
                             d="M13.6668 7.00065C13.6668 10.6825 10.6821 13.6673 7.00016 13.6673C3.31826 13.6673 0.333496 10.6825 0.333496 7.00065C0.333496 3.31875 3.31826 0.333984 7.00016 0.333984C10.6821 0.333984 13.6668 3.31875 13.6668 7.00065ZM7.00016 3.66732C7.36835 3.66732 7.66683 3.96579 7.66683 4.33398V7.66732C7.66683 8.03551 7.36835 8.33398 7.00016 8.33398C6.63197 8.33398 6.3335 8.03551 6.3335 7.66732V4.33398C6.3335 3.96579 6.63197 3.66732 7.00016 3.66732ZM7.00016 10.334C7.36835 10.334 7.66683 10.0355 7.66683 9.66732C7.66683 9.29913 7.36835 9.00065 7.00016 9.00065C6.63197 9.00065 6.3335 9.29913 6.3335 9.66732C6.3335 10.0355 6.63197 10.334 7.00016 10.334Z"
                           ></path>
                         </svg>
