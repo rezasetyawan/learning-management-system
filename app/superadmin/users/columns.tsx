4d3868788e3711b69ba7497528a3cb23042acf4f
rezasetyawan <setyawanreza960@gmail.com>
2024-03-04 11:23:56 +0700

    feat(superadmin users page): remove checkbox

--- app/superadmin/users/columns.tsx
+++ app/superadmin/users/columns.tsx
@@ -1,9 +1,8 @@
 "use client";
-import { ColumnDef } from "@tanstack/react-table";
-import { formatTimestampToShortString } from "@/utils";
 import { Button } from "@/components/ui/button";
+import { formatTimestampToShortString } from "@/utils";
+import { ColumnDef } from "@tanstack/react-table";
 import Link from "next/link";
-import { Checkbox } from "@/components/ui/checkbox";
 
 interface User {
   id: string;
@@ -17,26 +16,6 @@ interface User {
 }
 
 export const columns: ColumnDef<User>[] = [
-  {
-    id: "select",
-    header: ({ table }) => (
-      <Checkbox
-        checked={
-          table.getIsAllPageRowsSelected() ||
-          (table.getIsSomePageRowsSelected() && "indeterminate")
-        }
-        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
-        aria-label="Select all"
-      />
-    ),
-    cell: ({ row }) => (
-      <Checkbox
-        checked={row.getIsSelected()}
-        onCheckedChange={(value) => row.toggleSelected(!!value)}
-        aria-label="Select row"
-      />
-    ),
-  },
   {
     header: "No",
     cell: ({ row, table }) =>
